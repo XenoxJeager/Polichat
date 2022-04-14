aad1085aabe02697671e91d883c5734e59d83431
Fachi <azh@gmx.at>
2022-04-14 17:28:00 +0200

    added feature idea

--- frontend/src/pages/quiz/components/AnswerButton.tsx
+++ frontend/src/pages/quiz/components/AnswerButton.tsx
@@ -7,6 +7,7 @@ interface AnswerButtonProps {
     onClick: (weight: Weight) => void;
 }
 
+// ? color buttons corresponding to where you would move ?
 export const AnswerButton: React.FC<AnswerButtonProps> = ({text, weight, onClick}) => (
     <button onClick={() => onClick(weight)}>
         {text} {(weight.weightX).toFixed(3)} | {(weight.weightY).toFixed(3)}
