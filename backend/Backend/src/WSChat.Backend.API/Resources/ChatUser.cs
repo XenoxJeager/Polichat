78a6e590805948905f2d8131e547d84d36e2be85
Fachi <azh@gmx.at>
2022-04-15 17:51:52 +0200

    renamed to backend

---
+++ backend/Backend/src/WSChat.Backend.API/Resources/ChatUser.cs
@@ -0,0 +1,11 @@
+﻿using Polichat_Backend.Properties;
+using UGen;
+namespace Polichat_Backend
+{
+    public class ChatUser
+    {
+        public string Id = UGenerator.Generate();
+      
+        public Rooms Room = Rooms.Auth_Right;
+    }
+}
