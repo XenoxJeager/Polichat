e99eab4e43e3e1f3f1f7d30be623b921d1524232
Fachi <azh@gmx.at>
2022-04-15 17:53:04 +0200

    fixed inconsistency

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 start cmd /c "cd frontend && npm start"
-start cmd /C "cd backend/Backend/src/WSChat.Backend.API && dotnet run"
+start cmd /c "cd backend/Backend/src/WSChat.Backend.API && dotnet run"
