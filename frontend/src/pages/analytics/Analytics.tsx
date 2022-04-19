9ae39be9f91e2880208c100c8079becb98357fd2
Fachi <azh@gmx.at>
2022-04-19 19:49:30 +0200

    added components

--- frontend/src/pages/analytics/Analytics.tsx
+++ frontend/src/pages/analytics/Analytics.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 
+interface AnalyticsProps {
 
+}
 
-export class Analytics extends React.Component {
+interface Result {
+    x: number;
+    y: number;
+}
+
+interface AnalyticsData {
+    results: Result[];
+    activeChats: number;
+}
+
+interface AnalyticsState {
+    data?: AnalyticsData;
+}
+
+export class Analytics extends React.Component<AnalyticsProps, AnalyticsState> {
+    constructor(props: AnalyticsProps) {
+        super(props);
+        this.state = {}
+    }
+
     render(): React.ReactNode {
+        const loadIfNull = (value?: number): string => {
+            return value ? value.toString() : "Loading...";
+        };
+        const data = this.state.data;
+
         return (
             <>
-                <p>Some analytics here</p>
+                <p>Map</p>
             </>
         );
     }
