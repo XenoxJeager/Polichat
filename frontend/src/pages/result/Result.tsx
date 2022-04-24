2929e2c2dcb22b439b7e32bf85681eeb1b49db9d
Gam3s0nly <gamesonly3434@gmail.com>
2022-04-24 09:17:22 +0100

    unfinished endpoint

--- frontend/src/pages/result/Result.tsx
+++ frontend/src/pages/result/Result.tsx
@@ -3,32 +3,39 @@ import React from "react";
 import { getUrl } from "../../config/Constants";
 import { Plane } from "../quiz/Quiz";
 
+
 interface ResultProps {
     plane: Plane;
 }
 
+
+
 export default class Result extends React.Component<ResultProps> {
     constructor(props: ResultProps) {
         super(props);
+        
     }
 
     componentDidMount() {    
-        axios.post(getUrl("/evaluation"), 
-        {
-            x: this.props.plane.x,
-            y: this.props.plane.y
-        })
+        axios.post("http://localhost:3001/evaluation",)
         .then((response) => {
-            
-        });
+           const returnedData = response.data;
+           this.setState({
+               ideologies : returnedData,
+           })
+        })
+        .catch((reason) => {});
     }
 
 
     render(): React.ReactNode {
+        const ideology = this.state.ideologies.text;
+        const description = this.state.ideologies.text;
         return (
             <div>
                 <h1>Your Result is: {this.props.plane.x.toFixed(3)} | {this.props.plane.y.toFixed(3)}</h1>
-                <p>Some description</p>
+                <p>{ideology}</p>
+                <p>{description}</p>
             </div>
         );
     }
