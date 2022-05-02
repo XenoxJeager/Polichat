4bd5978fa15cc78466c6584f8a977f02848edd8c
spayc <74024033+spayc@users.noreply.github.com>
2022-05-02 12:51:05 +0200

    sign in page styled
    :)

--- frontend/src/pages/signIn/SignIn.tsx
+++ frontend/src/pages/signIn/SignIn.tsx
@@ -31,11 +31,41 @@ class SignIn extends React.Component<SignInProps, SignInState> {
     render(): React.ReactNode {
         return (
             <>
-                <p>Username: </p>
-                <input onChange={(event) => this.setState({username: event.target.value})}></input>
-                <p>Password: </p>
-                <input onChange={(event) => this.setState({password: event.target.value})}></input>
-                <button onClick={this.submitLogin.bind(this)}>Submit</button>
+                {/* <div className="justify-center flex">
+                    <p>Username: </p>
+                    <input onChange={(event) => this.setState({username: event.target.value})}></input>
+                    <p>Password: </p>
+                    <input onChange={(event) => this.setState({password: event.target.value})}></input>
+                    <button onClick={this.submitLogin.bind(this)}>Submit</button>
+                </div> */}
+
+                <div className="flex items-center justify-center min-h-screen bg-gray-20">
+                    <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
+                        <h3 className="text-2xl font-bold text-center">Sign In</h3>
+                        <form action="">
+                            <div className="mt-4">
+                                <div>
+                                    <label>Username</label>
+                                    <input type="text" placeholder="Username" 
+                                    className="w-full px-4 py-2 mt-2 rounded-5 bg-gray-50"
+                                    onChange={(event) => this.setState({username: event.target.value})}
+                                    />
+                                </div>
+                                <div className="mt-4">
+                                    <label>Password</label>
+                                    <input type="text" placeholder="Password" 
+                                    className="w-full px-4 py-2 mt-2 rounded-5 bg-gray-50"
+                                    onChange={(event) => this.setState({password: event.target.value})}
+                                    />
+                                </div>
+                                    <button onClick={this.submitLogin.bind(this)}
+                                    className="justify-center flex px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
+                                    >Submit</button>
+                            </div>
+                        </form> 
+                    </div>
+                </div>
+                
             </>
         );
     }
