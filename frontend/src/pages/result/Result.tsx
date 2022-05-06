20840d4fe52b2c72acf68987a1b3094c46de02c6
spayc <74024033+spayc@users.noreply.github.com>
2022-05-06 15:08:33 +0200

    fix commit
    :)

--- frontend/src/pages/result/Result.tsx
+++ frontend/src/pages/result/Result.tsx
@@ -94,17 +94,17 @@ export default class Result extends React.Component<ResultProps, ResultState> {
                     </div>
                     <div className="mx-14 my-5 text-center border-4 bg-gray-100">
                         <h1 className="text-3xl bold border-x-4 border-b-4 p-5">Chat with your comrades!</h1>
-                        <div className="border-x-4 p-5 ">
-                            <div className="bg-[url(" {car} ")] h-screen overscroll-y-auto overflow-y-scroll ">
-                                <div className="">
-                                <Chat vector={this.props.vector}/>
-                            </div> 
+                        <div className="h-screen border-x-4 p-5 overscroll-y overflow-y-scroll">
+                            <div className="flex justify-center">
+                                <div className="absolute">
+                                    <Chat vector={this.props.vector}/>
+                                </div> 
                             </div>
-                        </div>                               
+                        <img className="bg-repeat-y" src={car} alt="chad.png"></img> 
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
 }
-
